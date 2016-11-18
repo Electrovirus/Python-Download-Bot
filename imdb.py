0f94821d45aa25d95cd00e32b38dfad5f23159fc
Electrovirus <E.electrovirus@gmail.com>
2016-11-18 15:24:56 +0330

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -15,7 +15,7 @@ import urllib
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-Token = ''
+Token = '289743790:AAGbc4A88LL-bQGCiYyjB2dMblw2vvMFS3Y'
 owner = '184018132'
 bot = telebot.TeleBot(Token)
 
